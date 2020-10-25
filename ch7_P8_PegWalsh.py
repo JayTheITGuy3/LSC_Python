f21dcfedbe1f4c278beb158115bdd0f01837bf19
PegJWalsh <73369748+PegJWalsh@users.noreply.github.com>
2020-10-25 17:55:31 -0500

    Update ch7_P8_PegWalsh.py

--- ch7_P8_PegWalsh.py
+++ ch7_P8_PegWalsh.py
@@ -9,12 +9,20 @@ color image to sepia. Function should first call grayscale to
 convert the color image to grayscale.
 
 """
+"""
+
+Program: ch7_P8_PeggyWalsh.py
+
+Authors: 
+
+Description: Write and test a function named sepia that converts a
+color image to sepia. Function should first call grayscale to
+convert the color image to grayscale.
+
+"""
 from images import Image
 
-
 def grayscale(image):
-    image = Image("smokey.gif")
-    result = Image.new("RGB", image.getHeight, image.getWidth)
     for y in range(image.getHeight()):
         for x in range(image.getWidth()):
             (r, g, b) = image.getPixel(x, y)
@@ -23,11 +31,19 @@ def grayscale(image):
             b = int(b * 0.114)
             lum = r + g + b
             image.setPixel(x, y, (lum, lum, lum))
-            image.draw()
-  
-    
+            
+        
+def main(filename = "smokey.gif"):
+    image = Image(filename)
+    print("Close the image window to continue.")
+    image.draw()
+    grayscale(image)
+    print("Close the image window to quit.")
+    image.draw()
 
 
+if __name__ == "__main__":
+    main()
 
 ##def sepia(image, grayscale):
 ##     (red, green, blue) = image.getPixel(x, y)
@@ -41,10 +57,3 @@ def grayscale(image):
 ##        red = min(int(red * 1.08), 255)
 ##        blue = int(blue * 0.93)
 
-
-
-##def main(image):
-##    sepia()
-##    grayscale()
-##    main()
-        
